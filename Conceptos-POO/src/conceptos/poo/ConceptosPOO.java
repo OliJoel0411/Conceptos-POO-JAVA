ca67348e21bd833633be96f62d04acda4b6f4e95
Olijoel0411 <oliverloconlopez@gmail.com>
2022-09-20 11:45:06 -0600

    Iniciando proyecto

---
+++ Conceptos-POO/src/conceptos/poo/ConceptosPOO.java
@@ -0,0 +1,14 @@
+package conceptos.poo;
+
+/**
+ * @author oliver
+ */
+public class ConceptosPOO {
+    
+    public static void main(String[] args) {
+        /*
+        POO
+        */
+    }
+    
+}
