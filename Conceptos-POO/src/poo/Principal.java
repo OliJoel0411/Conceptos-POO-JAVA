5ac88d25e0580e7d3c588368776c4656e3d47fb1
Olijoel0411 <oliverloconlopez@gmail.com>
2022-09-26 09:46:34 -0600

    Actualizado desde Netbenas

--- Conceptos-POO/src/poo/Principal.java
+++ Conceptos-POO/src/poo/Principal.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// Sintaxis para definir el paquete en el que estamos trabajando 
 package poo;
 
 /**
  *
- * @author olive
+ * @author oliver
  */
+// Sintaxis para definir la clase principal
 public class Principal {
-    
+    // En la clase principal se ejercutara el main que sera el encargado de ejercutar el programa
+    public static void main(String[] args) {
+        
+    }
 }
